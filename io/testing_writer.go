371e2b2ed4c929b3f87365b3eb83d7e2f1e0d925
Salaheddine Namir <71770514+T3rryc@users.noreply.github.com>
2022-10-24 16:58:15 +0200

    Co-authored-by: elisegtr <elisegtr@users.noreply.github.com> Co-authored-by: RemyCHARLES <RemyCHARLES@users.noreply.github.com> Co-authored-by: Gregory-Pagnoux <Gregory-Pagnoux@users.noreply.github.com> Co-authored-by: Pierre Gorin <Pierre2103@users.noreply.github.com>

---
+++ io/testing_writer.go
@@ -0,0 +1,20 @@
+package io
+
+import (
+	"time"	
+)
+
+type TimeoutWriter struct {
+	duration time.Duration
+}
+
+func NewTimeoutWriter (duration time.Duration) *TimeoutWriter {
+	return &TimeoutWriter{
+		duration: duration,
+	}
+}
+
+func (w *TimeoutWriter) Writer(b []byte)(int, error){
+	time.Sleep(w.duration)
+	return len(b),nil
+}
